8d4bdc61b7a284b75cd8ca5d9cc684273dc6f04e
CharisseHao <charissehao@gmail.com>
2024-02-12 22:46:25 -0800

    load data

--- src/lib/App.svelte
+++ src/lib/App.svelte
@@ -1,8 +1,11 @@
 <script>
+    import { onMount } from 'svelte';
+    import * as d3 from 'd3';
+
     let tempData = [];
 
     onMount(async () => {
-        const res = await fetch('yelp_academic_dataset_review.json'); 
+        const res = await fetch('yelp_academic_dataset_business.csv'); 
         const csv = await res.text();
         tempData = d3.csvParse(csv, d3.autoType)
         console.log(tempData);
