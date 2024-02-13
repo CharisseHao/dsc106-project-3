7b1b5284fa3714969fcf62238fd25f3eff099952
Nicole Zhang <nwzhang@ucsd.edu>
2024-02-12 21:06:43 -0800

    add app and graph

---
+++ src/lib/Graph.svelte
@@ -0,0 +1,11 @@
+<script>
+	import * as d3 from 'd3';
+    <div class="visualization">
+        <svg width="500" height="500">
+            <g transform="translate(250, 120)">
+                <!-- Place for Pie -->
+            </g>
+        </svg>
+    </div>
+
+</script>
