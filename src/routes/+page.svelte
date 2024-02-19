9310978208ce69ceedb04960249e70f193885717
CharisseHao <charissehao@gmail.com>
2024-02-18 20:48:17 -0800

    fix title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <h1>
 Yelp Across America: Analyzing Business Distribution, Types, and Ratings Across States
+</h1>
 <body>
 Click a blue bar to drill down, or click the background to go back up.
 </body>
