1131876a7b96eb451f2afcfa942f5f077588fa20
CharisseHao <charissehao@gmail.com>
2024-02-18 22:46:50 -0800

    center text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
-<body>
-    <h1>
-    Yelp Across America: Analyzing Business Distribution, Types, and Ratings Across States
-    </h1>
-    <p> Click a blue bar to drill down, or click the background to go back up. </p>
-</body>
+<main>
+    <section>
+        <h1>
+        Yelp Across America: Analyzing Business Distribution, Types, and Ratings Across States
+        </h1>
+        <p> Click a blue bar to drill down, or click the background to go back up. </p>
+    </section>
+</main>
 
 <script>
     import App from '../lib/App.svelte';
@@ -11,16 +13,23 @@
 
 <App />
 
-
 <style>
     h1 {
+        font-family: "Times New Roman"; 
         font-size: 24px;
         color: #000;
     }
 
-    body {
-        font-family: sans-serif; 
+    section {
+        font-family: "Times New Roman"; 
         font-size: 16px; 
         color: #333;
+        text-align: center;
+    }
+
+    .chart-container {
+        display: inline-block;
+        border: 2px solid #333; /* Add border around the chart */
+        padding: 10px; /* Add padding around the chart */
     }
 </style>
